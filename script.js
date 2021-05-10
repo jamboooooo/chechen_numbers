5ca3aebdf27001efe6564a5e581dfe3b0e859cac
jambo <hisllam35@gmail.com>
2021-05-10 12:12:23 +0300

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let checNumbers = ['цхьаъ', 'шиъ', 'кхоъ', 'диъ', 'пхиъ', 'ялх', 'ворх1',
-    'барх1', 'исса', 'итта', 'цхьайта', 'шийта', 'кхойтта',
+    'барх1', 'исс', 'итт', 'цхьайта', 'шийта', 'кхойтта',
     'дейтта', 'пхийтта', 'ялхийтта', 'верх1итта', 'берхитта',
-    'ткъе ясса', 'ткъа', 'ткъе цхьаъ', 'ткъе шиъ', 'ткъе кхоъ',
-    'ткъе диъ', 'ткъе пхиъ', 'ткъе ялх', 'ткъе ворх1', 'ткъе барх',
-    'ткъе исса', 'ткъе итта'
+    'ткъе ясса', 'ткъа'
 ];
 
+let tensNumbers = ['ткъе', 'шоз ткъе', 'къуз ткъе', 'доьз ткъе', 'баь1а'];
+
+let hundredNumbers = ['б1е', 'ши б1е', 'кхо б1е', 'диъ б1е', 'пхи б1е', 'ялх б1е', 'ворх1 б1е', 'барх б1е', 'исс б1е']
+
 function getChechNumb(number) {
-    return checNumbers[number - 1];
+    let first = '';
+    let second = '';
+    if (number <= 20) {
+        return checNumbers[number - 1]
+    } else if (number < 100) {
+        first = Math.floor(Math.floor(number / 10) / 2) - 1;
+        return tensNumbers[first] + ' ' + checNumbers[number % 20 - 1]
+    } else if (number === 100) {
+        return tensNumbers[tensNumbers.length - 1]
+    } else if (number < 1000) {
+        first = Math.floor(number / 100) - 1;
+        second = Math.floor(Math.floor((number % 100) / 10) / 2) - 1;
+        return hundredNumbers[first] + ' ' + tensNumbers[second] + ' ' + checNumbers[(number % 100) % 20 - 1]
+    }
 }
 
-console.log(getChechNumb(5))
+console.log(getChechNumb(578))
